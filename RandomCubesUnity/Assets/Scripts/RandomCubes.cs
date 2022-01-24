dec1a49723d3fad8d2593682d6d932fb9b4c0062
beo8538 <beo8538@utulsa.edu>
2022-01-24 15:11:10 -0600

    Update RandomCubes
    Made code

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,29 @@
+/****
+* Created by: Betzaida Ortiz Rivas
+* Date Created: Jan 24, 2022
+* Last Edited by: nameof
+* Last Edited: Jan 24, 2022
+* 
+* Description: Spawn random cubes
+* 
+* ****/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
